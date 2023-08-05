cc8d8fd35d96c792f71c583ffbacacbfaa92f766
ElaDerVaTe <eladatia0021@gmail.com>
2023-08-05 17:32:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Product from './components/Product/Product';
+import { useState } from 'react';
+
 
 function App() {
+  const [data, setData] = useState([{name:'Shoes', price:300, img:''}, {name:'Pents', price:100, img:''}, {name:'Shirt', price:150, img:''}])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        data.map((item) =>{
+          return <Product name={item.name} price={item.price} image={item.img}/>
+        })
+      }
+
     </div>
   );
 }
---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+
+function Product(props) {
+  return (
+    <div>
+        <h3></h3>
+    </div>
+  )
+}
+
+export default Product
